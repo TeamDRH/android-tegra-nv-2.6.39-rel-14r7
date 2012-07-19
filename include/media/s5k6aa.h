f60efa9bce363ba0776adb79ae6282c4dd984a8c
Dane Wagner <dane.wagner@gmail.com>
2012-07-19 14:00:58 -0500

    Add some firmware register definitions for the S5K6AA camera.

--- include/media/s5k6aa.h
+++ include/media/s5k6aa.h
@@ -3,7 +3,20 @@
 
 #include <linux/ioctl.h>  /* For IOCTL macros */
 
-
+#define S5K6AA_R_FWdate 0x0134
+#define S5K6AA_R_FWapiVer 0x0136
+#define S5K6AA_R_FWrevision 0x0138
+#define S5K6AA_R_FWpid 0x013A
+#define S5K6AA_R_FWprjName 0x013C
+#define S5K6AA_R_FW_compDate 0x0148
+#define S5K6AA_R_FWSFC_VER 0x0154
+#define S5K6AA_R_FWTC_VER 0x0156
+#define S5K6AA_R_FWrealImageLine 0x0158
+#define S5K6AA_R_FWsenId 0x015A
+#define S5K6AA_R_FWusDevIdQaVersion 0x015C
+#define S5K6AA_R_usFwCompilationBits 0x015E
+#define S5K6AA_R_usSVNrevision 0x0160
+#define S5K6AA_R_SVNpathRomAddress 0x0164
 
 #define S5K6AA_IOCTL_SET_MODE		_IOW('o', 1, struct s5k6aa_mode)
 #define S5K6AA_IOCTL_GET_STATUS		_IOR('o', 2, __u8)
